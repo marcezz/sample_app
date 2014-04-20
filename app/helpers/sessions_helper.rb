af6ae4f6f65e921406a6997aa2f93a617f2d8095
Marcus Duttler <mduttler@gmail.com>
2014-04-20 20:08:58 +0200

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,12 +33,20 @@ module SessionsHelper
     	user == current_user
   	end
   	
-  	def sign_out
-    	current_user.update_attribute(:remember_token,
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+
+  def sign_out
+    current_user.update_attribute(:remember_token,
                                   User.digest(User.new_remember_token))
-    	cookies.delete(:remember_token)
-    	self.current_user = nil
-  	end
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
 
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
